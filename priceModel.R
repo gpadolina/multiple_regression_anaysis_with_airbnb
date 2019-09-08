bb700b9e6c2357bb7311b4583096b2dc9a425bd0
gpadolina <47703315+gpadolina@users.noreply.github.com>
2019-09-07 19:14:18 -0700

    Update priceModel.R

--- priceModel.R
+++ priceModel.R
@@ -15,3 +15,12 @@ summary(bostonAirbnb)
 
 # Some amenities are more important than others so I'll split them
 airbnbAmenities <- strsplit(bostonAirbnb$amenities, ",")
+
+# Remove url related variables because they are not needed for analysis
+bostonAirbnb[, names(bostonAirbnb)[grep("url", names(bostonAirbnb))]] <- NULL
+grep("url", names(bostonAirbnb))
+
+# Remove variables with duplicated values
+bostonAirbnb[, names(bostonAirbnb[(duplicated(t(bostonAirbnb)))])] <- NULL
+
+# Create dummy variables for important amenities
